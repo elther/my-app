66b8b6320d03efc5821f0710ab55ab9019459cd3
elther <elther@naver.com>
2021-12-23 00:11:52 +0900

    refactor : Condition.jsx - react condition

--- src/components/2-8.ConditionalRendering/Condition.jsx
+++ src/components/2-8.ConditionalRendering/Condition.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function UserGreeting(props){
-    return <h1>{props.name}, welcome it's {props.count}</h1>;
+    return <h1>{props.name && `${props.name},`} welcome {props.count ? `It's ${props.count} times` : null}</h1>;
 }
 
 function GuestGreeting(props){
@@ -13,10 +13,10 @@ function Greeting(props) {
     //     return <UserGreeting name="jimmy" count={0} />
     // }
     // return <GuestGreeting />;
-    return props.isLoggedIn ? <UserGreeting name="jimmy" count={0} /> : <GuestGreeting />
+    return props.isLoggedIn ? <UserGreeting name="timmy" count={0} /> : <GuestGreeting />
 }
 export default function Condition() {
-    const isLoggedIn = false;
+    const isLoggedIn = true;
 
     return (
         <div>
